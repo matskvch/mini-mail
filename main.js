5b128b86b95c9abb2c9596ba0fb6a1a2ad65c588
Yurii <matskevych.yura@gmail.com>
2025-01-14 23:16:52 +0200

    Create function for creating window

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path') // modules import
+
+async function createWindow() {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true, // adding node.js
+            preload: path.join(__dirname, 'preload.js') // connecting preload script
+        }
+    })
+    window.loadFile('src/index.html')
+}
