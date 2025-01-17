c7e7cf5026d8548732d24a305e670b01940a0a97
Yurii <matskevych.yura@gmail.com>
2025-01-17 21:45:04 +0200

    Small changes of code structure in composer.js

--- src/js/composer.js
+++ src/js/composer.js
@@ -1,34 +1,36 @@
 const composeEmailBtn = document.querySelector('.compose-btn') // link compose-btn and composeEmailBtn variable
 const composerWindow = document.querySelector('.composer-window') // save composer window into composerWindow variable
+const closeBtn = document.querySelector('.close-btn')
+const composerForm = document.querySelector('.composer-form') // all data added to composer
 
+ // при нажатии кнопки создать письмо откроется компоузер
 composeEmailBtn.addEventListener('click', () => {
     composerWindow.style.display = 'block';
-}) // при нажатии кнопки создать письмо откроется компоузер
+})
 
-const closeBtn = document.querySelector('.close-btn')
+// закрытие окна Х
 closeBtn.addEventListener('click', () => {
     composerWindow.style.display = 'none'
 })
 
+ // close composer button if clicked outside of composer
 composerWindow.addEventListener('click', (event) => {
     if (event.target === composerWindow) {
         composerWindow.style.display = 'none'
     }
-}) // close composer button if clicked outside of composer
-
-const composerForm = document.querySelector('.composer-form')
+})
 
 composerForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const emailTo = document.querySelector("#email-to").value;
+    const emailTo = document.querySelector("#email-to").value; // .value извлекает данные из инпутов
     const emailSubject = document.querySelector("#email-subject").value;
     const emailBody = document.querySelector(".email-body").value;
 
     if (emailTo.trim() === "" || emailSubject.trim() === "" || emailBody.trim() === "") {
         alert("Please fill in all fields");
         return;
-    }
+    } // в будущем можно оставить только проверку на поле TO:
 
     const newEmailData = {
         to: emailTo,
@@ -36,12 +38,10 @@ composerForm.addEventListener("submit", (event) => {
         body: emailBody,
     };
 
-    //console.log(newEmailData)
-
     addEmailToSent(newEmailData)
 
     composerForm.reset()
-    composerWindow.style.display = 'none'
+    composerWindow.style.display = 'none' // close composer window
 });
 
 function addEmailToSent(newEmailData) {
